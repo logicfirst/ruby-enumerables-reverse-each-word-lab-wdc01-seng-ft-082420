8fd7fc04089011a3b1e447322ac23c9baa89e589
Scott H Lee <slee@logicfirstllc.com>
2020-08-06 15:54:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,30 +1,13 @@
 
-#string = "Hello there, and how are you?"
-
 def reverse_each_word(string)
- #reverse_string = string.reverse.split.reverse.join(" ")
- #reverse_string_to_array = string.reverse.split
- string_to_array = string.split
- reverse_array = string_to_array.collect{ |element|
- element.reverse}
- 
- #reverse_string_to_array
- #reverse_string
- reverse_array.join(" ")
- #string_to_array
-end 
-#reverse_each_word(string)
+#reverse_string = string.reverse.split.reverse.join(" ")
+  new_array = string.reverse.split
+  reverse_array = []
+  reverse_array = new_array.collect{ |element| element.reverse }
+  
+  reverse_array.join(" ")
 
-
-#def reverse_each_word(string)
- #reverse_string = string.reverse.split.reverse.join(" ")
- #string_to_array = string.split
- #reverse_array = string_to_array.collect{ |element|
- #element.reverse}
- 
- #reverse_array.join(" ")
-#end 
-#reverse_each_word(string)
+end
 
 
   
